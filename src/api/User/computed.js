09223b7316fc8cc65b11d51860be96ffea079550
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-02-14 21:37:40 +0900

    #3.16 itsMe and amIFollowing Fileds part One

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -5,25 +5,27 @@ export default {
     fullName: parent => {
       return `${parent.firstName} ${parent.lastName}`;
     },
-    amIFollowing: async (parent, _, { request }) => {
+    isFollowing: async (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       try {
-        const exists = await prisma.$exists.user({
-          AND: [{ id: parentId }, { followers_some: [user.id] }]
+        return prisma.$exists.user({
+          AND: [
+            {
+              id: user.id
+            },
+            {
+              following_some: {
+                id: parentId
+              }
+            }
+          ]
         });
-        console.log(exists);
-        if (exists) {
-          return true;
-        } else {
-          return false;
-        }
-      } catch (error) {
-        console.log(error);
+      } catch {
         return false;
       }
     },
-    itsMe: (parent, _, { request }) => {
+    isSelf: (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       return user.id === parentId;
