dfbb55cd3222ce32750e32e9c97aff80424b5233
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-02-17 20:05:07 +0900

    #3.29 Conclusions

--- src/api/Message/newMessage/newMessage.js
+++ src/api/Message/newMessage/newMessage.js
@@ -5,16 +5,18 @@ export default {
     newMessage: {
       subscribe: (_, args) => {
         const { roomId } = args;
-        return prisma.$subscribe.message({
-          AND: [
-            { mutation_in: "CREATED" },
-            {
-              node: {
-                room: { id: roomId }
+        return prisma.$subscribe
+          .message({
+            AND: [
+              { mutation_in: "CREATED" },
+              {
+                node: {
+                  room: { id: roomId }
+                }
               }
-            }
-          ]
-        });
+            ]
+          })
+          .node();
       },
       resolve: payload => payload
     }
