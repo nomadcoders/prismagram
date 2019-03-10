02afd4474dcbd129926f399acea0c873df24820b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-03-10 14:55:07 +0900

    #5.8 confirmSecret + Log In Mutation

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,9 +4,16 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const exists = await prisma.$exists.user({ username });
+      const exists = await prisma.$exists.user({
+        OR: [
+          {
+            username
+          },
+          { email }
+        ]
+      });
       if (exists) {
-        throw Error("This username is already taken");
+        throw Error("This username / email is already taken");
       }
       await prisma.createUser({
         username,
