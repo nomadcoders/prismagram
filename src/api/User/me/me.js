0cab241c10b21780b37e25d2401bde810b8555cd
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-03-10 17:37:15 +0900

    Me resolvers

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -5,12 +5,7 @@ export default {
     me: async (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      const userProfile = await prisma.user({ id: user.id });
-      const posts = await prisma.user({ id: user.id }).posts();
-      return {
-        user: userProfile,
-        posts
-      };
+      return await prisma.user({ id: user.id });
     }
   }
 };
