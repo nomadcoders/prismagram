51336034a7cf5bfbecf72dff89d444a6d674f6f5
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-27 18:52:22 +0900

    Uploading to S3

--- src/upload.js
+++ src/upload.js
@@ -1,11 +1,32 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-const upload = multer({ dest: "uploads/" });
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
+  region: "ap-northeast-2"
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "prismagram",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    }
+  })
+});
+
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   const {
-    file: { path }
+    file: { location }
   } = req;
-  res.json({ path });
+  res.json({ location });
 };
